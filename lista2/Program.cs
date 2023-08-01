0d443bc74c4d13123da96c9ad5c3ab329b55d565
Azevedo <pedrohxpa@hotmail.com>
2023-08-01 14:27:03 -0400

    first commit

---
+++ lista2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        aluno1.Nome = "Elias";
+        aluno1.Cpf = "222";
+
+        Aluno aluno2 = new Aluno(11, "maria", "04666440275", new DateOnly(1995,07,22));
+
+        List<Aluno> alunos = new List<Aluno>();
+        alunos.Add(aluno1);
+        alunos.Add(aluno2);
+
+        foreach(Aluno a in alunos)
+        {
+            Console.WriteLine($"O nome é: {a.Nome}\nO cpf de {a.Nome} é: {a.Cpf}\n ");
+        }
+    }
+}
